e9cc151e9313ee6b2fe2657a05951484aaa1c203
tawakalmit <tawakal.mit@gmail.com>
2025-03-21 09:57:48 +0700

    test commit

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,6 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="text-4xl fixed top-0 right-0 bottom-0 left-0 bg-[#2c3e50] text-[#ecf0f1] flex items-center justify-center">Website Makintaw in Maintenance</div>
+    <div className="text-4xl fixed top-0 right-0 bottom-0 left-0 bg-[#2c3e50] text-[#ecf0f1] flex items-center justify-center">Website Makintaw Is in Maintenance</div>
   );
 }
